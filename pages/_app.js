72dc01e69862ca38b28ec8ef0af5619d8f80fded
SantilliZammit <santillizammit@gmail.com>
2026-03-24 12:10:30 -0400

    show cart

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { CartProvider } from "../context/CartContext";
+import CartDrawer from "../components/CartDrawer";
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      <CartDrawer />
       <Component {...pageProps} />
     </CartProvider>
   );
