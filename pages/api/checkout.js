3ffcd4b4244c73735731fa4c63815afea48a00eb
SantilliZammit <santillizammit@gmail.com>
2026-03-24 11:31:04 -0400

    add stripe session id to success redirect

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
       payment_method_types: ["card"],
       line_items,
       mode: "payment",
-      success_url: `${req.headers.origin}/success`,
+      success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/cart`,
     });
 
